bf9c3b2f6826d0f5b8fce272a44caa4ddd4eb1f5
ReinderKas <reinderkas1997@gmail.com>
2024-08-16 22:01:46 +0200

    Hmm

---
+++ Udemy/Angular/README.md
@@ -0,0 +1,16 @@
+# Requisits
+### Installation:
+ - Node js <b3>
+ - Angular Cli <b3>
+
+
+## Learned so far:
+ - Components should be seen as a tree. <br>
+
+ ``` html
+<router-outlet />
+```
+equals
+ ``` html
+<router-outlet></router-outlet>
+```
---
+++ Udemy/Angular/UdemyApp/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy_users';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterOutlet, 
+    HeaderComponent,
+    UserComponent
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'UdemyApp';
+  users = DUMMY_USERS;
+}
